988ae84396d98339c4700cbfae76c7bbe454eb6b
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-07-19 00:24:15 -0400

    player movement + leafwing input (#1)
    * player movement + leafwing input
    
    * fix

--- src/player.rs
+++ src/player.rs
@@ -1,1 +1,79 @@
 use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+
+pub struct SpawnPlayerEvent;
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Component)]
+pub struct GridPosition(pub IVec2);
+
+#[derive(Actionlike, Clone)]
+pub enum PlayerAction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugin(InputManagerPlugin::<PlayerAction>::default())
+            .add_event::<SpawnPlayerEvent>()
+            .add_system(player_controller)
+            .add_startup_system(setup);
+    }
+}
+
+fn setup(mut cmd: Commands) {
+    spawn_player(&mut cmd)
+}
+
+fn spawn_player(cmd: &mut Commands) {
+    let input_map = InputMap::new([
+        (KeyCode::Left, PlayerAction::Left),
+        (KeyCode::A, PlayerAction::Left),
+        (KeyCode::Right, PlayerAction::Right),
+        (KeyCode::D, PlayerAction::Right),
+        (KeyCode::Up, PlayerAction::Up),
+        (KeyCode::W, PlayerAction::Up),
+        (KeyCode::Down, PlayerAction::Down),
+        (KeyCode::S, PlayerAction::Down),
+    ]);
+
+    let e = cmd.spawn().id();
+
+    cmd.entity(e)
+        .insert(Player)
+        .insert(GridPosition(IVec2::ZERO))
+        .insert_bundle(InputManagerBundle::<PlayerAction> {
+            action_state: ActionState::default(),
+            input_map,
+        });
+}
+
+fn player_controller(mut cmd: Commands, query: Query<&ActionState<PlayerAction>, With<Player>>) {
+    if let Ok(action_state) = query.get_single() {
+        let mut dir = IVec2::ZERO;
+
+        if action_state.just_pressed(PlayerAction::Left) {
+            dir += IVec2::new(-1, 0);
+        }
+        if action_state.just_pressed(PlayerAction::Right) {
+            dir += IVec2::new(1, 0);
+        }
+        if action_state.just_pressed(PlayerAction::Up) {
+            dir += IVec2::new(0, -1);
+        }
+        if action_state.just_pressed(PlayerAction::Down) {
+            dir += IVec2::new(0, 1);
+        }
+
+        if dir != IVec2::ZERO {
+            info!("{}", dir);
+        }
+    }
+}
