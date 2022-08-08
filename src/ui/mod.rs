87c350fa206840fd1b5fdc55b53b1a8c7372303d
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-08 19:35:53 -0400

    projectile animations (#37)
    * projectile animations
    
    * spawn event
    
    * arrow sprite
    
    * projectile rotation

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,12 +1,13 @@
 pub mod attack_animation;
 pub mod attack_indicator;
 pub mod move_indicator;
+pub mod projectile;
 
 use bevy::prelude::*;
 
 use self::{
     attack_animation::AttackAnimationPlugin, attack_indicator::AttackIndicatorPlugin,
-    move_indicator::MoveIndicatorPlugin,
+    move_indicator::MoveIndicatorPlugin, projectile::ProjectilePlugin,
 };
 
 pub struct UIPlugin;
@@ -15,6 +16,7 @@ impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(AttackIndicatorPlugin)
             .add_plugin(AttackAnimationPlugin)
-            .add_plugin(MoveIndicatorPlugin);
+            .add_plugin(MoveIndicatorPlugin)
+            .add_plugin(ProjectilePlugin);
     }
 }
---
+++ src/ui/projectile.rs
@@ -0,0 +1,96 @@
+use bevy::prelude::*;
+use bevy_bobs::{
+    component::lifetime::*,
+    physics_2d::{PhysicsPlugin, RigidBody},
+};
+
+use crate::{
+    assets::SpriteSheet,
+    grid::to_world_coords,
+    utils::{to_rotation, Dir},
+};
+
+const CELL_TYPE: u32 = 8;
+
+#[derive(Component)]
+pub struct Projectile;
+
+pub struct SpawnProjectileEvent {
+    pub sprite_index: usize,
+    pub spawn_pos: IVec2,
+    pub dir: Dir,
+    pub distance: u32,
+    pub speed: f32,
+}
+
+pub struct ProjectilePlugin;
+
+impl Plugin for ProjectilePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugin(PhysicsPlugin)
+            .add_event::<SpawnProjectileEvent>()
+            .add_system(duration_lifetime_system)
+            .add_system(distance_lifetime_system)
+            .add_system(spawn)
+            .add_system(despawn)
+            .add_system(debug);
+    }
+}
+
+fn spawn(
+    mut cmd: Commands,
+    mut events: EventReader<SpawnProjectileEvent>,
+    asset_sheet: Res<SpriteSheet>,
+) {
+    use std::f32::consts::PI;
+
+    for SpawnProjectileEvent {
+        sprite_index,
+        spawn_pos,
+        dir,
+        distance,
+        speed,
+    } in events.iter()
+    {
+        cmd.spawn_bundle(SpriteSheetBundle {
+            sprite: TextureAtlasSprite {
+                index: *sprite_index,
+                ..default()
+            },
+            texture_atlas: asset_sheet.clone(),
+            transform: Transform {
+                translation: to_world_coords(spawn_pos).extend(2.),
+                rotation: Quat::from_rotation_z(to_rotation(*dir) - PI / 2.),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(DistanceLifetime::new(*distance as f32))
+        .insert(RigidBody {
+            mass: 1.,
+            velocity: *speed * IVec2::from(*dir).as_vec2(),
+            ..default()
+        })
+        .insert(Projectile);
+    }
+}
+
+fn despawn(mut cmd: Commands, query: Query<(Entity, &DistanceLifetime), With<Projectile>>) {
+    for (entity, distance_lifetime) in query.iter() {
+        if distance_lifetime.is_expired() {
+            cmd.entity(entity).despawn();
+        }
+    }
+}
+
+fn debug(mut writer: EventWriter<SpawnProjectileEvent>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::E) {
+        writer.send(SpawnProjectileEvent {
+            sprite_index: 39,
+            spawn_pos: IVec2::new(4, 4),
+            dir: Dir::North,
+            distance: 5 * CELL_TYPE,
+            speed: 200.,
+        });
+    }
+}
