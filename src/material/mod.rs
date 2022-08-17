d493da8cadf777a96745e6d2e11a1387201c1df6
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-16 23:53:45 -0400

    outline shader start

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -1,7 +1,13 @@
-use bevy::prelude::*;
+pub mod outline;
+
+use bevy::{prelude::*, sprite::Material2dPlugin};
+
+use self::outline::OutlineMaterial;
 
 pub struct MaterialPlugin;
 
 impl Plugin for MaterialPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_plugin(Material2dPlugin::<OutlineMaterial>::default());
+    }
 }
---
+++ src/material/outline.rs
@@ -0,0 +1,16 @@
+use bevy::{
+    prelude::*,
+    reflect::TypeUuid,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    sprite::Material2d,
+};
+
+#[derive(AsBindGroup, TypeUuid, Clone)]
+#[uuid = "d0a41df4-03f6-4af3-be33-d042ffc81a11"]
+pub struct OutlineMaterial {}
+
+impl Material2d for OutlineMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/outline_material.wgsl".into()
+    }
+}
